de4029f0a8a6f0e038db9d18e31b5616abd8d71d
Hung Nguyen <97866339+hungqng@users.noreply.github.com>
2022-07-07 09:54:47 -0700

    Add files via upload

--- Perfect Number.py
+++ Perfect Number.py
@@ -8,3 +8,12 @@ class Solution:
     def checkPerfectNumber(self, num: int) -> bool:
         return num in (6, 28, 496, 8128, 33550336)
 
+        # Solution 2
+        if num == 1:
+            return False
+        res = 1
+        for i in range(2,int(num**0.5)+1):
+            if num%i == 0:
+                res += i + num//i
+        return res == num
+
